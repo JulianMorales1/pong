99bbcc789d247e62d9592c871d8355bceee3e8d8
JulianMorales1 <98353349+JulianMorales1@users.noreply.github.com>
2022-03-25 22:13:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let ball = document.querySelector('.ball')
+let ballVelocityX = 7;
+let ballVelocityY = 7;
+let ballPositionX = 90;
+let ballPositionY = 90;
+
+
 // Size of the game area (in px)
 const GAME_AREA_WIDTH = 700;
 const GAME_AREA_HEIGHT = 500;
@@ -18,13 +25,47 @@ let computerPaddleYVelocity = 1;
 
 // Update the pong world
 function update() {
+    ballPositionX = ballPositionX - ballVelocityX;
+    ball.style.left = ballPositionX + 'px';
+    
+    ballPositionY = ballPositionY - ballVelocityY;
+    ball.style.top = ballPositionY + 'px';
+    
+    //ballVelocityY += .5;
+    
+    //ballVelocityX += .2;
 
+    //bottom edge boundary
+    if(ballPositionY > 480){
+        ballVelocityY *= -1;
+        ballPositionY = 480;
+    }
+
+    //top edge boundary
+    if(ballPositionY <= 0){
+        ballVelocityY *= -1;
+        ballPositionY = 0;
+    }
+
+    //left edge boundary
+    if(ballPositionX <= 0){
+        ballVelocityX *= -1;
+        ballPositionX = 0;
+    }
+    // right edge boundary
+    if(ballPositionX > 680){
+        ballVelocityX *= -1;
+        ballPositionX = 680;
+    }
+}
+
+    
     // Update the computer paddle's position
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
-}
+
 
 // Call the update() function every 35ms
 setInterval(update, 35);
